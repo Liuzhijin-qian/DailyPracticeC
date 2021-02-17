5d0fa670af641ef63d94db1d0335450cf95a6bfb
Sunmoonliker <1473120002@qq.com>
2021-02-17 22:31:15 +0800

    Mail-List

--- FileOperations.c
+++ FileOperations.c
@@ -1,169 +1,169 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-
-//int main()
-//{
-//	//打开文件
-//	FILE* pfWrite = fopen("test.txt", "w");
-//	if (pfWrite == NULL)
-//	{
-//		printf("%s", strerror(errno));
-//	}
-//	//写入文件
-//	fputc('L', pfWrite);
-//	fputc('2', pfWrite);
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//#include <errno.h>
 //
-//
-//	//关闭文件
-//	fclose(pfWrite);
-//	pfWrite = NULL;
-//	return 0;
-//}
-
-
-//int main()
-//{
-//	//打开要读的文件
-//	FILE* pfRead = fopen("test.txt", "r");
-//	if (pfRead == NULL)
-//	{
-//		printf("%s", strerror(errno));
-//	}
-//
-//	//把要读取的文件打印到屏幕上
-//	printf("%c", fgetc(pfRead));//一次只读一个字符
-//	printf("%c", fgetc(pfRead));
-//	printf("%c", fgetc(pfRead));
-//
-//	//关闭文件
-//	fclose(pfRead);
-//	pfRead = NULL;
-//
-//
-//	return 0;
-//}
-
-
-//
-//int main()
-//{
-//	char buf[1024] = { 0 };
-//	//打开要复制的文件
-//	FILE* pf = fopen("test.txt", "r");
-//	if (pf == NULL)
-//	{
-//		printf("%s", strerror(errno));
-//
-//	}
-//	//把读取到的数据放到数组里面
-//	fgets(buf, 1024, pf);
-//	printf("%s", buf);
-//
-//	//关闭文件
-//	fclose(pf);
-//	pf = NULL;
-//
-//
-//	return 0;
-//}
-//
-
-
-//int main()
-//{
-//	//以写的形式打开文件
-//	FILE* pf = fopen("text.txt", "w");
-//	if (pf == NULL)
-//	{
-//		printf("%s", strerror(errno));
-//	}
-//
-//
-//	//写入文件
-//	fputs("liuqianwochaojiaini", pf);
-//
-//	//关闭文件
-//	fclose(pf);
-//	pf = NULL;
-//	return 0;
-//}
-//
-//
-//
-//int main()
-//{
-//	char buf[1024];
-//	gets(buf);
-//	puts(buf);
-//
-//	return 0;
-//}
-
-
-
-//struct test
-//{
-//	int n;
-//	float df;
-//	char arr[10];
-//
-//};
-//int main()
-//{
-//	struct test Con = {0};
-//	//打开文件
-//	FILE* pf = fopen("text.txt", "r");
-//	fscanf(pf, "%d %f %s", &(Con.n), &(Con.df), Con.arr);
-//	printf("%d %f %s", Con.n, Con.df, Con.arr);
-//
-//
-//	fclose(pf);
-//	pf = NULL;
-//
-//
-//
-//	return 0;
+////int main()
+////{
+////	//打开文件
+////	FILE* pfWrite = fopen("test.txt", "w");
+////	if (pfWrite == NULL)
+////	{
+////		printf("%s", strerror(errno));
+////	}
+////	//写入文件
+////	fputc('L', pfWrite);
+////	fputc('2', pfWrite);
+////
+////
+////	//关闭文件
+////	fclose(pfWrite);
+////	pfWrite = NULL;
+////	return 0;
 ////}
+//
+//
+////int main()
+////{
+////	//打开要读的文件
+////	FILE* pfRead = fopen("test.txt", "r");
+////	if (pfRead == NULL)
+////	{
+////		printf("%s", strerror(errno));
+////	}
+////
+////	//把要读取的文件打印到屏幕上
+////	printf("%c", fgetc(pfRead));//一次只读一个字符
+////	printf("%c", fgetc(pfRead));
+////	printf("%c", fgetc(pfRead));
+////
+////	//关闭文件
+////	fclose(pfRead);
+////	pfRead = NULL;
+////
+////
+////	return 0;
+////}
+//
+//
+////
+////int main()
+////{
+////	char buf[1024] = { 0 };
+////	//打开要复制的文件
+////	FILE* pf = fopen("test.txt", "r");
+////	if (pf == NULL)
+////	{
+////		printf("%s", strerror(errno));
+////
+////	}
+////	//把读取到的数据放到数组里面
+////	fgets(buf, 1024, pf);
+////	printf("%s", buf);
+////
+////	//关闭文件
+////	fclose(pf);
+////	pf = NULL;
+////
+////
+////	return 0;
+////}
+////
+//
+//
+////int main()
+////{
+////	//以写的形式打开文件
+////	FILE* pf = fopen("text.txt", "w");
+////	if (pf == NULL)
+////	{
+////		printf("%s", strerror(errno));
+////	}
+////
+////
+////	//写入文件
+////	fputs("liuqianwochaojiaini", pf);
+////
+////	//关闭文件
+////	fclose(pf);
+////	pf = NULL;
+////	return 0;
+////}
+////
+////
+////
+////int main()
+////{
+////	char buf[1024];
+////	gets(buf);
+////	puts(buf);
+////
+////	return 0;
+////}
+//
+//
+//
+////struct test
+////{
+////	int n;
+////	float df;
+////	char arr[10];
+////
+////};
+////int main()
+////{
+////	struct test Con = {0};
+////	//打开文件
+////	FILE* pf = fopen("text.txt", "r");
+////	fscanf(pf, "%d %f %s", &(Con.n), &(Con.df), Con.arr);
+////	printf("%d %f %s", Con.n, Con.df, Con.arr);
+////
+////
+////	fclose(pf);
+////	pf = NULL;
+////
+////
+////
+////	return 0;
+//////}
+////
+////
+////struct S
+////{
+////	int n;
+////	float lg;
+////	char arr[20];
+////};
+////
+////int main()
+////{
+////	struct S s = { 10,3.14,"liuqian" };
+////	char buf[1024];
+////	FILE* pf = fopen("test.txt", "w");
+////
+////	fprintf(pf, "%d %f %s", s.n, s.lg, s.arr);
+////
+////
+////	return 0;
+////}
+//
 //
 //
 //struct S
 //{
+//	char name[10];
 //	int n;
 //	float lg;
-//	char arr[20];
 //};
 //
 //int main()
 //{
-//	struct S s = { 10,3.14,"liuqian" };
-//	char buf[1024];
-//	FILE* pf = fopen("test.txt", "w");
-//
-//	fprintf(pf, "%d %f %s", s.n, s.lg, s.arr);
-//
+//	//struct S s = { "张三",20,3.14 };
+//	struct S tmp;
+//	FILE* pf = fopen("test.txt", "rb");
+//	//fwrite(&s, sizeof(s), 1, pf);
+//	fread(&tmp, sizeof(tmp), 1, pf);
+//	printf("%s %d %f \n", tmp.name,tmp.n,tmp.lg);
 //
 //	return 0;
 //}
-
-
-
-struct S
-{
-	char name[10];
-	int n;
-	float lg;
-};
-
-int main()
-{
-	//struct S s = { "张三",20,3.14 };
-	struct S tmp;
-	FILE* pf = fopen("test.txt", "rb");
-	//fwrite(&s, sizeof(s), 1, pf);
-	fread(&tmp, sizeof(tmp), 1, pf);
-	printf("%s %d %f \n", tmp.name,tmp.n,tmp.lg);
-
-	return 0;
-}
